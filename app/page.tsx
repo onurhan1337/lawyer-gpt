50b6bdcb688d98dafbd9293bfe20ec18186deab5
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-06-08 07:47:22 +0300

    feat: add next auth with github provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { signIn } from "./auth";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello World</p>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("github");
+        }}
+      >
+        <button
+          type="submit"
+          className="bg-black text-white px-4 py-2 rounded-md"
+        >
+          Sign in with GitHub
+        </button>
+      </form>
     </main>
   );
 }
