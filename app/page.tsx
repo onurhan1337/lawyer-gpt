61a04056100771dcd3ba692bd367aeaaba9c4448
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-06-09 16:24:06 +0300

    fix: update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,52 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import Link from "next/link";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
+  const cookeStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookeStore });
 
-  const isSupabaseConnected = canInitSupabaseClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
+    <div className="w-full flex flex-col items-center">
+      <div className="flex flex-col gap-14 max-w-4xl px-3 py-16 lg:py-24 text-foreground">
+        <div className="flex flex-col items-center mb-4 lg:mb-12">
+          <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
+          <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center my-12">
+            Chat with your files using <strong>Supabase</strong> and{" "}
+            <strong>Next.js</strong>
+          </p>
+          {user ? (
+            <div className="flex flex-row gap-2">
+              <Link
+                href="/files"
+                className="bg-foreground py-3 px-6 rounded-lg font-mono text-sm text-background"
+              >
+                Upload
+              </Link>
+              <Link
+                href="/chat"
+                className="bg-foreground py-3 px-6 rounded-lg font-mono text-sm text-background"
+              >
+                Chat
+              </Link>
+            </div>
+          ) : (
+            <div className="flex flex-row gap-2">
+              <Link
+                href="/login"
+                className="bg-foreground py-3 px-6 rounded-lg font-mono text-sm text-background"
+              >
+                Login
+              </Link>
+            </div>
+          )}
         </div>
-      </nav>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
+        <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent" />
       </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
