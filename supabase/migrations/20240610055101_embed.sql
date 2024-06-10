6ff2fae56c908e10034043a39dc4f2f1808a8a4a
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-06-10 09:29:18 +0300

    feat: Add embed trigger function for document_sections table

---
+++ supabase/migrations/20240610055101_embed.sql
@@ -0,0 +1,39 @@
+create function private.embed()
+returns trigger
+language plpgsql
+as $$
+declare
+  content_column text = TG_ARGV[0];
+  embedding_column text = TG_ARGV[1];
+  batch_size int = case when array_length(TG_ARGV, 1) >= 3 then TG_ARGV[2]::int else 5 end;
+  timeout_milliseconds int = case when array_length(TG_ARGV, 1) >= 4 then TG_ARGV[3]::int else 5 * 60 * 1000 end;
+  batch_count int = ceiling((select count(*) from inserted) / batch_size::float);
+begin
+  -- Loop through each batch and invoke an edge function to handle the embedding generation
+  for i in 0 .. (batch_count-1) loop
+  perform
+    net.http_post(
+      url := supabase_url() || '/functions/v1/embed',
+      headers := jsonb_build_object(
+        'Content-Type', 'application/json',
+        'Authorization', current_setting('request.headers')::json->>'authorization'
+      ),
+      body := jsonb_build_object(
+        'ids', (select json_agg(ds.id) from (select id from inserted limit batch_size offset i*batch_size) ds),
+        'table', TG_TABLE_NAME,
+        'contentColumn', content_column,
+        'embeddingColumn', embedding_column
+      ),
+      timeout_milliseconds := timeout_milliseconds
+    );
+  end loop;
+
+  return null;
+end;
+$$;
+
+create trigger embed_document_sections
+  after insert on document_sections
+  referencing new table as inserted
+  for each statement
+  execute procedure private.embed(content, embedding, 10);
