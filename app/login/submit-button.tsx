f600991af6faa49fddc42ce924dfb601dfac6dbd
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-06-12 15:17:11 +0300

    fix: update import for useFormStatus in submit-button.tsx

--- app/login/submit-button.tsx
+++ app/login/submit-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { type ComponentProps } from "react";
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 type Props = ComponentProps<"button"> & {
   pendingText?: string;
