3149bd278f6820c4d46874dded6d506815224d25
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-06-16 11:37:25 +0300

    feat: add login server action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,61 +1,17 @@
-import { createClient } from "@/utils/supabase/server";
-import { headers } from "next/headers";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { redirect } from "next/navigation";
-import { SubmitButton } from "./submit-button";
+import { login } from "./actions";
 
 export default function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
-  const signIn = async (formData: FormData) => {
-    "use server";
-
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/files");
-  };
-
-  const signUp = async (formData: FormData) => {
-    "use server";
-
-    const origin = headers().get("origin");
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/login?message=Check email to continue sign in process");
-  };
-
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
         href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+        className="absolute left-8 top-12 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -74,7 +30,10 @@ export default function Login({
         Back
       </Link>
 
-      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+      <form
+        id="login-form"
+        className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+      >
         <label className="text-md" htmlFor="email">
           Email
         </label>
@@ -97,20 +56,9 @@ export default function Login({
           placeholder="******"
           required
         />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-teal-700 text-white rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
+        <Button formAction={login} className="h-10">
           Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
-          Sign Up
-        </SubmitButton>
+        </Button>
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
             {searchParams.message}
